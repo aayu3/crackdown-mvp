b3e6e495a1bafd767a8d8b07a20f4fc249a8e270
Aaron Yu <aaron.yu@ucsf.edu>
2025-05-29 14:38:42 -0700

    Working login and logout functionality

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,5 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 //const analytics = getAnalytics(app);
